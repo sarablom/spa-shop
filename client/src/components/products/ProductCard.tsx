6bb1382c9b24ac7110e9a9c462dd8c430b036097
sarablom <sara.cecilia.blom@gmail.com>
2022-02-11 08:46:19 +0100

    add some styling

--- client/src/components/products/ProductCard.tsx
+++ client/src/components/products/ProductCard.tsx
@@ -1,9 +1,7 @@
-
 import { Product } from "../../models/Product";
 
 import styled from "styled-components";
 import { COLORS } from "../../styles/constants";
-
 
 interface Props {
   products: Product[] | [];
@@ -13,21 +11,23 @@ interface Props {
 function ProductCard(props: Props) {
   return (
     <>
-    <ListWrapper>
-      {props.products &&
-        props.products.map((product: Product, index) => (
-          <ListItem key={product.id + index}>
-            <img src={product.imgUrl} alt={product.title} />
-            <h3>{product.title}</h3>
-            <hr />
-            <p>{product.description}</p>
-            <p>{product.price}</p>
-            <button onClick={() => props.addToCartHandler(product)}>
-              Lägg i kundkorg
-            </button>
-          </ListItem>
-        ))}
-    </ListWrapper>
+      <ListWrapper>
+        {props.products &&
+          props.products.map((product: Product, index) => (
+            <ListItem key={product.id + index}>
+              <div>
+                <img src={product.imgUrl} alt={product.title} />
+                <h3>{product.title}</h3>
+                <hr />
+                <p>{product.description}</p>
+                <p>{product.price}</p>
+              </div>
+              <button onClick={() => props.addToCartHandler(product)}>
+                Lägg i kundkorg
+              </button>
+            </ListItem>
+          ))}
+      </ListWrapper>
     </>
   );
 }
@@ -43,25 +43,36 @@ const ListWrapper = styled.ul`
 `;
 
 const ListItem = styled.li`
+  display: flex;
+  flex-direction: column;
   max-width: 300px;
+  min-height: 100%;
   border: 1px solid ${COLORS.darkBrown};
   border-radius: 8px;
   padding: 1.5rem;
   margin: 0 auto;
   background: ${COLORS.lightBrown};
+  div {
+    flex: 1;
 
-  img {
-    width: 100%;
-    border-radius: 4px 4px 0 0;
+    img {
+      width: 100%;
+      border-radius: 4px 4px 0 0;
+    }
+
+    h3,
+    p {
+      padding: 8px 0;
+    }
+
+    hr {
+      border-top: 1px solid ${COLORS.darkBrown};
+    }
   }
 
-  h3,
-  p {
-    padding: 8px 0;
-  }
-
-  hr {
-    border-top: 1px solid ${COLORS.darkBrown};
+  button {
+    padding: .5rem 1rem;
+    cursor: pointer;
   }
 `;
 
--- client/src/styles/constants.ts
+++ client/src/styles/constants.ts
@@ -1,11 +1,13 @@
 export const COLORS = {
-    primary: 'rgb(102, 153, 102)',
-    secondary: 'rgb(186, 127, 82)',
-    lightGreen: 'rgb(153, 204, 153)',
-    mediumGreen: 'rgb(51, 153, 51)',
-    darkGreen: 'rgb(51, 102, 51)',
-    lightBrown: 'rgb(204, 169, 146)',
-    mediumBrown: 'rgb(153, 91, 36)',
-    darkBrown: 'rgb(107, 60, 27)',
-    black: 'rgba(0, 0, 0, 0.6)',
-  };
+  primary: "rgb(102, 153, 102)",
+  secondary: "rgb(186, 127, 82)",
+  extraLightGreen: "rgb(220, 234, 220)",
+  lightGreen: "rgb(153, 204, 153)",
+  mediumGreen: "rgb(51, 153, 51)",
+  darkGreen: "rgb(51, 102, 51)",
+  extraLightBrown: "rgb(234, 222, 216)",
+  lightBrown: "rgb(204, 169, 146)",
+  mediumBrown: "rgb(153, 91, 36)",
+  darkBrown: "rgb(107, 60, 27)",
+  black: "rgba(0, 0, 0, 0.6)",
+};
