c3661faa4b5b6ea08e55430a4fb0f294f2cd4673
sarablom <sara.cecilia.blom@gmail.com>
2022-02-16 16:24:17 +0100

    add tests for filtered search

--- client/src/components/search/test/FilterSearch.test.tsx
+++ client/src/components/search/test/FilterSearch.test.tsx
@@ -1,12 +1,28 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import FilterSearch from "../FilterSearch";
-import { product } from "../../../dummyData";
+import { products } from "../../../dummyData";
 
 const setFilteredProductsMock = jest.fn();
-const productTestData = [product];
+const productTestData = products;
 
 describe("FilterSearch component", () => {
     it("renders without crashing", () => {
         render(<FilterSearch products={productTestData} setFilteredProducts={setFilteredProductsMock} />)
     })
+
+    it("displays an input element", () => {
+        render(<FilterSearch products={productTestData} setFilteredProducts={setFilteredProductsMock} />)
+    
+        expect(screen.getByRole("textbox")).toBeInTheDocument();
+      });
+
+      it('starts filtering when user is typing', () => {
+        render(<FilterSearch products={productTestData} setFilteredProducts={setFilteredProductsMock} />)
+    
+        const inputElem = screen.getByRole("textbox");
+        userEvent.type(inputElem, 'mass');
+    
+        expect(setFilteredProductsMock).toHaveBeenCalledTimes(4);
+      });
 })
