08e6472a256562766bbe7b4798eafe8ccef366d5
sarablom <sara.cecilia.blom@gmail.com>
2022-03-31 07:15:06 +0200

    add protections against brute force, xss- attacks and no-sql injections

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitizer = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 const path = require("path");
 const mongoose = require("mongoose");
 const productRoutes = require("./routes/productRoutes");
@@ -14,8 +18,18 @@ const DATABASE_USER = process.env.REACT_APP_MONGOUSER;
 const app = express();
 
 //Middlewares
+app.use(helmet());
 app.use(cors());
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: "Too many requests from this IP, please try again in an hour!"
+  });
+  
+app.use("/api", limiter);
 app.use(express.json());
+app.use(mongoSanitizer());
+app.use(xss());
 
 //Routes
 app.use("/api/products", productRoutes);
